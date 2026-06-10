41b0eaf1669e4d5bb019175a987187bdea2b3752
adsonlima98 <adsonlima85@gmail.com>
2026-06-10 14:10:47 -0300

    par chave-valor para tabela hash

---
+++ Entrada.java
@@ -0,0 +1,17 @@
+
+public class Entrada<K, V> {
+
+    K key;                   
+    V value;               
+    Entrada<K, V> proximo;  
+    public Entrada(K key, V value) {
+        this.key = key;
+        this.value = value;
+        this.proximo = null;
+    }
+
+    @Override
+    public String toString() {
+        return "{" + key + "=" + value + "}";
+    }
+}
